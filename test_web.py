a0859a914575adce929786d162228a86707a9158
AnastasiIns <78545789+AnastasiIns@users.noreply.github.com>
2023-04-14 11:32:57 +0300

    Add files via upload

---
+++ test_web.py
@@ -0,0 +1,8 @@
+﻿import pytest
+
+
+@pytest.mark.usefixtures("setup")
+class Tests:
+    def test_title(self):
+        self.driver.get('https://www.bmw.ru/ru/index.html')
+        assert self.driver.title == "BMW - официальный сайт в России"
